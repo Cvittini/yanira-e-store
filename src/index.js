8b64cb64799a70ffc39f1edca24c225e2cf4345f
Cristian Vittini <cvittini@gmail.com>
2025-04-21 22:52:27 -0400

    tittle change

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ParallaxProvider } from "react-scroll-parallax";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <ParallaxProvider>
     <App />
-  </React.StrictMode>
+  </ParallaxProvider>,
+  document.getElementById('root')
 );
-
-
